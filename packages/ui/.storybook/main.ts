57f7860e653cf18b0e2adb5cf49769be17c4ae96
haeun <110523397+haruyam15@users.noreply.github.com>
2025-06-07 17:03:39 +0900

    [✨Feat] 입력 컴포넌트 개발 #8 (#31)
    * feat: input 컴포넌트 구현 (#8)
    
    * feat: input컴포넌트 구현(#8)
    
    * feat: input컴포넌트 구현(#8)
    
    * feat: textarea 컴포넌트 개발(#8)
    
    ---------
    
    Co-authored-by: choiyoungae <109134495+choiyoungae@users.noreply.github.com>

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,19 +1,15 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-docs"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-onboarding', '@storybook/addon-docs'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
   viteFinal: async (config) => {
+    config.plugins = [...(config.plugins || []), tsconfigPaths()];
     config.css = {
       postcss: {
         plugins: [
